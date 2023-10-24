5b19f746362dc15d679dbda70fc7ac0d7cd96ee6
John S Sobieski <johnso515@protonmail.com>
2023-10-24 09:29:52 -0500

    Updated Source/Classes/ItemLocation.ps1 to support remote locations since Directory/File class does not survive deserialization

--- Source/Classes/ItemLocation.ps1
+++ Source/Classes/ItemLocation.ps1
@@ -2,5 +2,16 @@ class ItemLocation {
     <# Define the class. Try constructors, properties, or methods. #>
     [string]$Drive
     [System.IO.DirectoryInfo]$Path
-    [string]$RemotePath
+    [string]$PathString
+    [string]$ComputerName    # The name of the computer where the path/drive exists
+
+    [string] GetPathString () {
+        <# Action to perform. You can use $ to reference the current instance of this class #>
+        if ($null -eq $this.Path) {
+            return $this.PathString
+        }
+        else {
+            return $this.Path.FullName
+        }
+    }
 }
